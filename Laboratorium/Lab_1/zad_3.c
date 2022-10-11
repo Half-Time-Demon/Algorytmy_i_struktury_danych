96aae971ae1efe0e05c8565101231dd05b1eb7f5
Half-Time-Demon <72053859+Half-Time-Demon@users.noreply.github.com>
2022-10-12 01:02:11 +0300

    Update zad_3.c

--- Laboratorium/Lab_1/zad_3.c
+++ Laboratorium/Lab_1/zad_3.c
@@ -14,46 +14,35 @@ X(n)
 #include <stdlib.h>
 #include <time.h>
 
-float X;
-void obliczenia(float X[], float a, int n);
 int validate(float);
+// float X[];
 
 int main() {
   int n = 0;
-  float a = 0, x = 0;
+  float a = 0, x = 0, y;
+
+   do {
+    printf("Podaj stala a=");
+    scanf("%f", &a);
+  } while (a == 0);
+
   do {
-    printf("wprowadz liczba a=");
-    scanf(" %f", &a);
-    printf("rozmiar tablicy n=");
-    scanf(" %d", &n);
-    float X[n];
-    for (int i = 0; i < n; i++) {
-      printf("wprowadz liczba X(%d)=", i + 1);
-      scanf("%f", &x);
-      X[i] = x;
-    }
-  } while (1 != validate(a));
-  obliczenia(&X, a, n);
-  return 0;
-}
+    printf("Podaj rozmiar tablicy X(n)=");
+    scanf("%d", &n);
+  } while (n < 0);
 
-int validate(float a) {
-  if (a == 0) {
-    printf("something wrong\n");
-    return 0;
-  } else {
-    printf("200OK\n");
-    return 1;
+  float X[n];
+  for (int i = 0; i < n; i++) {
+    printf("wprowadz liczba X(%d)=", i + 1);
+    scanf("%f", &x);
+    X[i] = x;
   }
-}
+  
 
-void obliczenia(float X[], float a, int n) {
-  int i = 0;
-  float y, x;
-  while (i <= n) {
+  for (int i = 0; i < n; i++) {
     printf("iteracja %d: x=%.1f a=%.1f ", i + 1, X[i], a);
     y = a * pow(X[i], 2);
     printf("y=%.1f\n", y);
-    i++;
   }
+  return 0;
 }
