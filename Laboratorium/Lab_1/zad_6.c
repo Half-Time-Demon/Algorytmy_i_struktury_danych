5f5ba6cda86eb01ed6bacaa5cfd3926912503e09
Half-Time-Demon <72053859+Half-Time-Demon@users.noreply.github.com>
2022-10-12 02:10:58 +0300

    Create zad_6.c

---
+++ Laboratorium/Lab_1/zad_6.c
@@ -0,0 +1,63 @@
+/*
+Daniel Trojko, Student
+* -----------------
+ Laboratorium 1. Zadanie przyklad
+ Obliczyc funkcja y = ax^2 znaczenia, gdy a - stale, x jest wybierany z tablicy
+X(n)
+* -----------------
+2022-10-11
+*/
+// gcc zad_5.c -o Zad_5 -lm
+
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int validate(float);
+
+int main() {
+  int n = 0;
+  float a = 0, x = 0, y;
+
+  do {
+    printf("Podaj stala a=");
+    scanf("%f", &a);
+  } while (a == 0);
+
+  do {
+    printf("Podaj rozmiar tablicy X(n)=");
+    scanf("%d", &n);
+  } while (n < 0);
+
+  float X[n];
+  float Y[n];
+
+  for (int i = 0; i < n; i++) {
+    printf("wprowadz liczba X(%d)=", i + 1);
+    scanf("%f", &x);
+    X[i] = x;
+  }
+
+  for (int i = 0; i < n; i++) {
+    printf("iteracja %d: x=%.1f a=%.1f ", i + 1, X[i], a);
+    Y[i] = a * pow(X[i], 2);
+    printf("y=%.1f\n", Y[i]);
+  }
+
+  int suma = 0, ilosc = 0, moznik = 1, i = 0;
+
+  ilosc = sizeof(Y) / sizeof(Y[0]);
+  for (int i = 0; i <= ilosc; i++) {
+    suma = suma + Y[i];
+    moznik = moznik * Y[i];
+  }
+
+  printf("ilosc=%d\n", ilosc);
+  printf("suma=%d\n", suma);
+  printf("moznik=%d\n", moznik);
+
+  printf("srednia=%d\n", ilosc / suma);
+
+  return 0;
+}
