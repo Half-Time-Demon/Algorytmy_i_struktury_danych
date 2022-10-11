0fc9765569b84ac492dd595a33597a9359b903dd
Half-Time-Demon <72053859+Half-Time-Demon@users.noreply.github.com>
2022-10-11 22:29:43 +0300

    Create zad_3.c

---
+++ Laboratorium/Lab_1/zad_3.c
@@ -0,0 +1,56 @@
+/*
+Daniel Trojko, Student
+* -----------------
+ Laboratorium 1. Zadanie przyklad
+ Obliczyc funkcja y = ax^2 znaczenia, gdy a - stale, x jest wybierany z tablicy
+X(n)
+* -----------------
+2022-10-11
+*/
+
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void obliczenia(float, float, float, int);
+int validate(float);
+
+int main() {
+  int x = 0, n = 0;
+  float a = 0;
+  do {
+    printf("wprowadz liczba a=");
+    scanf(" %f", &a);
+    printf("rozmiar tablicy n=");
+    scanf(" %d", &n);
+    float X[n];
+    for (int i = 0; i < n; i++) {
+      printf("wprowadz liczba X(%d)=", i + 1);
+      scanf(" %d", &x);
+      X[i] = x;
+    }
+  } while (1 != validate(*X[], a, n));
+  obliczenia(*X[], a, n);
+  return 0;
+}
+
+int validate(float a) {
+  if (a == 0) {
+    printf("something wrong");
+    return 0;
+  } else {
+    printf("200OK");
+    return 1;
+  }
+}
+
+void obliczenia(float *X[], float a, float hx, int xn) {
+  float y;
+  while (x <= xn) {
+    printf("x=%.1f a=%.1f ", x, a);
+    y = a * pow(x, 2);
+    x = x + hx;
+    printf("y=%.1f\n", y);
+  }
+}
