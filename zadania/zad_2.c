230c2168ef41e671a7bc3e71b0a0992998ff800d
Half-Time-Demon <72053859+Half-Time-Demon@users.noreply.github.com>
2022-10-25 21:56:19 +0300

    Create zad_2.c

---
+++ zadania/zad_2.c
@@ -0,0 +1,24 @@
+# Zadanie 2
+## pseudocod Iloczyn i reszta. Realizacja z warunkem pocztkawym calkowita liczba - n, wyjsce licby(lista)
+```
+int iloczyn_i_reszta(liczba)
+{
+    int r,q,dzielnik=2;
+    int lista[1][liczba/2];
+
+    while(dzielnik<liczba/2){
+        r=liczba%dzielnik;
+        q=liczba/dzielnik;
+        lista[0][dzielnik-2]=r;
+        lista[1][dzielnik-2]=q;
+        dzielnik++;
+    }
+
+    return lista;
+}
+
+```
+
+## Udowodnienie
+```
+```
