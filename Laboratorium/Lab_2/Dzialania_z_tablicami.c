50b0f15e9e8763ebd5f9c5c355adb8062efb4c30
Half-Time-Demon <72053859+Half-Time-Demon@users.noreply.github.com>
2022-10-25 23:12:06 +0300

    Update Dzialania_z_tablicami.c

--- Laboratorium/Lab_2/Dzialania_z_tablicami.c
+++ Laboratorium/Lab_2/Dzialania_z_tablicami.c
@@ -21,10 +21,8 @@ int findMin(int *arr);
 
 void printTable(int *arr);
 
-#include <stdio.h>
-
 void main(void) {
-  float arr[100];
+  int arr[100];
   // realizacja
 
   srand(time(NULL)); // use current time as seed for random generator
@@ -34,8 +32,7 @@ void main(void) {
     arr[i] = rand();
     printf("%f ", arr[i]);
   }*/
-  int i = 1;
-  if (find(&arr) == 1) {
+  if (find(arr) == 1) {
     printf("find true\n");
   } else {
     printf("find false\n");
@@ -45,18 +42,60 @@ void main(void) {
 
 // Funkcii
 
-int size(int *arr) { return (sizeof(arr) / sizeof(&arr[0])); }
+int size(int *arr) { return (sizeof(arr) / sizeof(arr[0])); }
 
 int find(int *arr) {
   // int *arr2 = *arr;
-  if (size(&arr) > 0) {
+  printf("%d", size(arr));
+  if (size(arr) > 0) {
     int value = 5;
-    for(int i=0;i<size(arr);i++){
-      if(arr[i]==value){
-        printf("%d\n",arr[i]);
-        //return 1;
+    for (int i = 0; i < size(arr); i++) {
+      printf("%d\n", arr[i]);
+      if (arr[i] == value) {
+        return 1;
       }
     }
   }
   return 0;
+}
+
+void insert(int item, int pos, int *arr) { arr[pos] = item; }
+
+void remove2(int pos, int *arr) {
+  int tmp_arr[size(arr) - 1];
+  for (int i = 1; i < size(arr); i++) {
+    if (i != pos) {
+      tmp_arr[i] = arr[i];
+    } else {
+      i++;
+      tmp_arr[i--] = arr[i];
+    }
+  }
+  *arr = *tmp_arr;
+}
+
+int findMax(int *arr) {
+  int max = arr[0];
+  for (int i = 0; i < size(arr); i++) {
+    if (max < arr[i]) {
+      max = arr[i];
+    }
+  }
+  return max;
+}
+
+int findMin(int *arr) {
+  int min = arr[0];
+  for (int i = 0; i < size(arr); i++) {
+    if (min > arr[i]) {
+      min = arr[i];
+    }
+  }
+  return min;
+}
+
+void printTable(int *arr) {
+  for (int i = 0; i < size(arr); i++) {
+    printf(" %d ", arr[i]);
+  }
 }
